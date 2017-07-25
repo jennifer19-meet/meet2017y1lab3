f2b3625ae09efd2479b814701f11b88e192bc851
jennifer19-meet <jennifer19@meet.mit.edu>
2017-07-25 10:01:48 +0300

    Stopping here

---
+++ specialCharacters.py
@@ -0,0 +1,6 @@
+print (" \"Hi!\" ")
+print (" \'Hello!\' ")
+print (" \\MEET\\Module1\\")
+print ("first \n second")
+print ("no tab \t tab")
+print("Instructor Alex asks, \"What are you learning today?\" \n The students reply, \'We are learning how to print!\' We can type print (“\\Day1\\”) to print out \Day1\.")
